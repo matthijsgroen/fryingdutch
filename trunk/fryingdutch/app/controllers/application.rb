7d29285c3ea7c8cb0cbc83bfdf661c5368a83bf2
matthijs.groen <matthijs.groen@6fd87d96-9615-11dd-9f2b-5b14b159ea90>
2008-10-09 15:35:40 +0000

    Initial import

---
+++ trunk/fryingdutch/app/controllers/application.rb
@@ -0,0 +1,15 @@
+# Filters added to this controller apply to all controllers in the application.
+# Likewise, all the methods added will be available for all controllers.
+
+class ApplicationController < ActionController::Base
+  helper :all # include all helpers, all the time
+
+  # See ActionController::RequestForgeryProtection for details
+  # Uncomment the :secret if you're not using the cookie session store
+  protect_from_forgery # :secret => 'ca9769fca6c5fc26e0d28bd4b083bddb'
+  
+  # See ActionController::Base for details 
+  # Uncomment this to filter the contents of submitted sensitive data parameters
+  # from your application log (in this case, all fields with names like "password"). 
+  # filter_parameter_logging :password
+end
