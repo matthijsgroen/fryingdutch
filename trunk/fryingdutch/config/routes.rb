ea9662365e73c7942fbe7c36481b3147d133914e
matthijs.groen <matthijs.groen@6fd87d96-9615-11dd-9f2b-5b14b159ea90>
2008-10-10 06:27:24 +0000

    Start game list, games tagcloud

--- trunk/fryingdutch/config/routes.rb
+++ trunk/fryingdutch/config/routes.rb
@@ -1,4 +1,7 @@
 ActionController::Routing::Routes.draw do |map|
+  map.resources :games
+  map.games_by_tagname "games/tag/:tagname", :controller => "games", :action => "by_tagname"
+
   map.resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
--- trunk/fryingdutch/db/schema.rb
+++ trunk/fryingdutch/db/schema.rb
@@ -9,7 +9,28 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20081009134532) do
+ActiveRecord::Schema.define(:version => 20081009160603) do
+
+  create_table "games", :force => true do |t|
+    t.string   "name"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "taggings", :force => true do |t|
+    t.integer  "tag_id",        :limit => 11
+    t.integer  "taggable_id",   :limit => 11
+    t.string   "taggable_type"
+    t.datetime "created_at"
+  end
+
+  add_index "taggings", ["tag_id"], :name => "index_taggings_on_tag_id"
+  add_index "taggings", ["taggable_id", "taggable_type"], :name => "index_taggings_on_taggable_id_and_taggable_type"
+
+  create_table "tags", :force => true do |t|
+    t.string "name"
+  end
 
   create_table "users", :force => true do |t|
     t.string   "username"
