260f2ed132c3768d37fac3ba536199ef803db6f1
matthijs.groen <matthijs.groen@6fd87d96-9615-11dd-9f2b-5b14b159ea90>
2008-10-21 08:13:57 +0000

    Started game rating

--- trunk/fryingdutch/app/models/user.rb
+++ trunk/fryingdutch/app/models/user.rb
@@ -8,7 +8,7 @@ class User < ActiveRecord::Base
   has_many :games_playing, :class_name => "Game", :through => :user_games_playing, :source => :game
 
   def before_save
-    self.permalink = "#{nickname.gsub(/[^a-z0-9]+/i, '-')}".downcase if self.nickname
+    self.permalink = "#{id}-#{nickname.gsub(/[^a-z0-9]+/i, '-')}".downcase if self.nickname
   end
 
   def to_param
--- trunk/fryingdutch/public/javascripts/application.js
+++ trunk/fryingdutch/public/javascripts/application.js
@@ -1,5 +1,19 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+GameRating = $.klass({
+	initialize: function() {
+		alert("test");
+	},
+	onclick: function(event) {
+		this.element.html("");
+	}	
+	
+});
+
+
 $(function() {
+
+	$(".gamerating").attach(GameRating)
 	
 });
