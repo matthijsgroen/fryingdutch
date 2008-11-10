5f784bd8e4f7513e9b1485ff897159a33beeb87e
Matthijs Groen <matthijs.groen@gmail.com>
2008-11-10 11:54:59 +0000

    more functionality tests for games

--- fryingdutch/test/functional/games_controller_test.rb
+++ fryingdutch/test/functional/games_controller_test.rb
@@ -6,9 +6,10 @@ class GamesControllerTest < ActionController::TestCase
     get :index
     assert_response :success
     assert_not_nil assigns(:games)
+    assert_select "div.game h2 a", "Travian"
   end
 
-  def test_should_not_get_new
+  def test_no_access_new
     get :new
     assert_login_required
   end
@@ -20,13 +21,26 @@ class GamesControllerTest < ActionController::TestCase
     assert_response :success
   end
 
+  def test_no_access_create_game
+    post :create, :game => { }
+    assert_login_required
+  end
+
+  def test_invalid_game_values
+    login_as :thaisi
+    assert_difference('Game.count', 0) do
+      post :create, :game => { }
+    end
+    assert_response :success
+    assert_select "div.fieldWithErrors", :minimum => 2
+  end
+
   def test_should_create_game
-    
-#    assert_difference('Game.count') do
-#      post :create, :game => { }
-#    end
-#
-#    assert_redirected_to game_path(assigns(:game))
+    login_as :thaisi
+    assert_difference('Game.count', 1) do
+      post :create, :game => { :name => "Test game", :description => "this game is cool", :tag_list => "test, game, browser" }
+    end
+    assert_redirected_to games_path
   end
 
   def test_should_show_game
@@ -34,9 +48,22 @@ class GamesControllerTest < ActionController::TestCase
     assert_response :success
   end
 
+  def test_should_show_by_tagname
+    get :by_tagname, :tagname => "space"
+    assert_response :success
+    assert_not_nil assigns(:games)
+    assert_select "div.game h2 a", "Planetarion"
+  end
+  
+  def test_should_not_get_edit
+    get :edit, :id => games(:games_001).permalink
+    assert_login_required
+  end
+
   def test_should_get_edit
-#    get :edit, :id => games(:one).id
-#    assert_response :success
+    login_as :thaisi
+    get :edit, :id => games(:games_001).permalink
+    assert_response :success
   end
 
   def test_should_update_game
