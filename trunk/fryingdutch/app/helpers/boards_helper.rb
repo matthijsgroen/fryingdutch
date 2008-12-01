9283583d484eaf422ae3f7c3f87c22d7c9ac58b2
matthijs.groen <matthijs.groen@6fd87d96-9615-11dd-9f2b-5b14b159ea90>
2008-12-01 12:23:48 +0000

    minor board changes

--- trunk/fryingdutch/app/helpers/boards_helper.rb
+++ trunk/fryingdutch/app/helpers/boards_helper.rb
@@ -1,2 +1,12 @@
 module BoardsHelper
+  
+  def bread_crumbs(board)
+    items = []
+    while board = board.parent       
+      items << link_to(h(board.name), board)      
+    end   
+    items << link_to("alle boards", boards_path)
+    items.reverse.join " \\ "
+  end
+
 end
