cc26418ffbd4f6bec0527f8e4d2f7560e66891cd
matthijs.groen <matthijs.groen@6fd87d96-9615-11dd-9f2b-5b14b159ea90>
2008-12-08 13:16:53 +0000

    Start boards and topics, table layout (just for now, later we'll make it more web 2.0ish)

---
+++ trunk/fryingdutch/app/controllers/boards/topics_controller.rb
@@ -0,0 +1,21 @@
+class Boards::TopicsController < ApplicationController
+
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def delete
+  end
+
+end
--- trunk/fryingdutch/app/models/board.rb
+++ trunk/fryingdutch/app/models/board.rb
@@ -2,6 +2,9 @@ class Board < ActiveRecord::Base
   
   belongs_to :parent, :class_name => "Board"
   has_many :children, :foreign_key => "parent_id", :class_name => "Board", :order => "position"
+  
+  has_many :topics, :class_name => "Comment", :as => :comment_on, :conditions => { :category => "topic" }, :order => "created_at DESC", :dependent => :destroy
+  
   has_permalink
   acts_as_list :scope => 'parent_id #{parent_id.nil? ? " IS NULL" : " = #{parent_id}"}'
   named_scope :root_boards, :conditions => { :parent_id => nil }, :order => "position"
