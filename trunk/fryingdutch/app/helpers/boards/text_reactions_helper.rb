4fa525b4a901996edead546581a4ea903a69847e
matthijs.groen@gmail.com <matthijs.groen@gmail.com@6fd87d96-9615-11dd-9f2b-5b14b159ea90>
2009-01-16 08:55:27 +0000

    Interface flow of board improvements, adding in AJAX functionality

--- trunk/fryingdutch/app/helpers/boards/text_reactions_helper.rb
+++ trunk/fryingdutch/app/helpers/boards/text_reactions_helper.rb
@@ -1,4 +1,5 @@
 module Boards::TextReactionsHelper
 
-  
+  include BoardsHelper
+ 
 end
--- trunk/fryingdutch/app/helpers/boards/topics_helper.rb
+++ trunk/fryingdutch/app/helpers/boards/topics_helper.rb
@@ -7,17 +7,33 @@ module Boards::TopicsHelper
   def board_boards_discussion_topic_path(board, topic)
     board_discussion_topic_path(board, topic)
   end
-
-  include BoardsHelper
-  
-  alias :board_bread_crumbs :bread_crumbs 
-  
-  def bread_crumbs(topic)
+   
+  def bread_crumbs_for(*parts)
     items = []
-    items << board_bread_crumbs(topic.comment_on)
-    items << link_to_unless_current(h(topic.content.title), [topic.comment_on, topic.content])
-    items.join " &gt; "
-  end
+    parts = [parts] unless parts.class == Array 
     
-  
+    board, topic, reaction = parts[0], parts[1], parts[2] if parts.size == 3 
+    board, topic = parts[0], parts[1] if parts.size == 2 
+    board = parts[0] if parts.size == 1 
+    
+    if reaction
+      items << "Reactie toevoegen" if reaction.new_record?
+      items << "Reactie bewerken" unless reaction.new_record?
+    end
+    
+    if topic
+      items << link_to_unless_current(h(topic.content.title), [topic.comment_on, topic.content])
+    end
+
+    if board
+      items << link_to_unless_current(h(board.name), board)      
+      while board = board.parent       
+        items << link_to_unless_current(h(board.name), board)      
+      end  
+    end
+    
+    items << link_to_unless_current("Alle fora", boards_path)
+    items.reverse.join " &gt; "
+  end
+   
 end
